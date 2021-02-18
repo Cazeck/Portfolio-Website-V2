44fb6a4b7e15aa481b19b505b6a0b4e952826ee5
Cazeck <caseykolodziejczyk@gmail.com>
2021-02-17 23:43:52 -0600

    Implementing original portfolio website

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    #return render_template("index.html")
-    return "Hello, Flask!"
+    return render_template("index.html")
+    #return "Hello, Flask!"
 
 if __name__=="__main__":
     app.run(debug=True)
